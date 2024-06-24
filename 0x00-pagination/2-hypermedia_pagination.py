89f671d805d0ac9dad262e4b763885c8a1773d85
KenNyagz <kennyagz681@gmail.com>
2024-06-24 14:27:49 +0300

    Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+'''Server class to paginate a database of popular baby names.
+and get a particular page
+'''
+import csv
+import math
+from typing import Tuple, Union, List, Dict, Any
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''gets specified page'''
+        assert isinstance(page, int) and page > 0, "page must be an integer\
+                                                    greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must\
+                                            be an integer greater than 0"
+        if page > len(self.dataset()) or page_size > len(self.dataset()):
+            return []
+        start, end = self.index_range(page, page_size)
+        page = self.dataset()[start: end]
+        return page
+
+    def index_range(self, page, page_size):
+        '''return a tuple of size two containing a start and an end index'''
+        starting_point = (page * page_size) - page_size
+        end_point = page * page_size
+
+        return (starting_point, end_point)
+
+    def get_hyper(self, page: int = 1, page_size: int = 10):
+        '''returns different metadata related to the paginated data'''
+        dataset = self.dataset()
+        tot_pages = math.ceil(len(self.dataset()) / page_size)
+        dicto = {"page_size": page_size,
+                 "page": page,
+                 "data": self.get_page(page, page_size),
+                 "next_page": None if page + 1 > tot_pages else page + 1,
+                 "prev_page": None if page - 1 <= 0 else page - 1,
+                 "total_page": tot_pages}
+        return dicto
