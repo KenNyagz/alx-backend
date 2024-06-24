233a1311650ee2b0abeba874168c85696fa0d40d
KenNyagz <kennyagz681@gmail.com>
2024-06-24 13:54:43 +0300

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,13 +1,46 @@
 #!/usr/bin/env python3
+'''Server class to paginate a database of popular baby names.
+and get a particular page
 '''
-return a tuple of size two containing a start index and an end index
-'''
-from typing import Tuple, Union
+import csv
+import math
+from typing import Tuple, Union, List
 
 
-def index_range(page, page_size):
-    '''return a tuple of size two containing a start index and an end index'''
-    starting_point = (page * page_size) - page_size
-    end_point = page * page_size
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
 
-    return (starting_point, end_point)
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''gets specified page'''
+        assert isinstance(page, int) and page > 0, "page must be an integer\
+                                                    greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must\
+                                            be an integer greater than 0"
+        if page > len(self.dataset()) or page_size > len(self.dataset()):
+            return []
+        start, end = self.index_range(page, page_size)
+        page = self.dataset()[start: end]
+        return page
+
+    def index_range(self, page, page_size):
+        '''return a tuple of size two containing a start and an end index'''
+        starting_point = (page * page_size) - page_size
+        end_point = page * page_size
+
+        return (starting_point, end_point)
