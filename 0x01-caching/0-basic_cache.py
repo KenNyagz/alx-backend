43e43cd7ee744e93d4b4a6fd27c7d50adfcb7246
KenNyagz <kennyagz681@gmail.com>
2024-06-26 00:41:14 +0300

    saving to and retrieving from cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+cache class inheritance
+'''
+#import
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''inherits from baseCaching to add more caching functionality'''
+    # def __init__(self):
+        # '''constructor method'''
+        # super().__init__()
+
+    def put(self, key, item):
+        '''Add an item to the cache'''
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''Get an item by key'''
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
