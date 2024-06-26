579246606eecab35aa4c86ee30aebc1e19746e20
KenNyagz <kennyagz681@gmail.com>
2024-06-26 22:57:15 +0300

    MRU Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -2,7 +2,6 @@
 '''
 Caching that uses the Least Recently Used mechanism
 '''
-from collections import OrderedDict
 from base_caching import BaseCaching
 
 
@@ -11,25 +10,29 @@ class MRUCache(BaseCaching):
     def __init__(self):
         '''init/constructor method'''
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.mru_order = []
 
     def put(self, key, item):
         '''Add an item by key'''
         if key is None or item is None:
             return
 
-        if key in self.cache_data:
-            self.cache_data.move_to_end(key)
-
         self.cache_data[key] = item
 
+        if key in self.mru_order:
+            self.mru_order.remove(key)
+        self.mru_order.append(key)
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            most_recent_key = next(reversed(self.cache_data))
+            # most_recent_key = next(reversed(self.cache_data))
+            most_recent_key = self.mru_order.pop(0)
+            del self.cache_data[most_recent_key]
             print(f"DISCARD: {most_recent_key}")
-            self.cache_data.pop(most_recent_key)
 
     def get(self, key):
         '''Get an item key'''
         if key is None or key not in self.cache_data:
             return None
-        return self.cache_data.get(key, None)
+        self.mru_order.remove(key)
+        self.mru_order.append(key)
+        return self.cache_data.get(key)
