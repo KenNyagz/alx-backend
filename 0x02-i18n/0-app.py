6fed4452bea4dc179fa470d027efaa96e49c65c9
KenNyagz <kennyagz681@gmail.com>
2024-07-02 17:45:12 +0300

    basic falsk app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+basic flask app that with just '/' route that sends output to index.html
+'''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
